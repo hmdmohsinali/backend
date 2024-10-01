92f3c9d71ac0901db9f3a0916c473f9b1922bb5d
Mohsin A. <hmdmohsinali@gmail.com>
2024-10-01 11:41:12 +0500

    cors fixed

--- app.js
+++ app.js
@@ -6,11 +6,17 @@ import { connectDB } from './config/db.js';
 
 dotenv.config();
 
-// Connect to MongoDB
-connectDB();
-const PORT = process.env.PORT || 5000;
-
 const app = express();
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+
+connectDB();
+
 
 // Middleware to parse JSON
 app.use(express.json());
