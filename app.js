ff78f211ce19c194b771c039e580fe13d306eb6a
Mohsin A. <hmdmohsinali@gmail.com>
2024-10-01 11:20:27 +0500

    deploment error testing

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/cars', carRoutes);
 
-app.listen(PORT, () => {
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
+}
+
+export default app;
